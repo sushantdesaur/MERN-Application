f9af1699f868b4c78dada6d436ad2bd8cb51a8cb
sushantdesaur <desaursushant@gmail.com>
2022-01-05 02:16:57 +0530

    * created  logout function * implemented useNavigate from react roter dom * implemented useDispatch from  react redux

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,22 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
-
+import { useDispatch } from 'react-redux';
 import useStyles from './styles'
 import memories from '../../images/memories.png';
 
 const Navbar = () => {
     const classes = useStyles();
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
     
-    console.log(user);
+    const logout = () => {
+        dispatch({ type: 'LOGOUT'});
+        navigate('/');
+        setUser(null);
+    };
+    // useEffect(() => {
+    //     const token = user?.token;
 
-    useEffect(() => {
-        const token = user?.token;
-
-        // JWT ...
-        setUser(JSON.parse(localStorage.getItem('profile')));
-    }, []);
+    //     // JWT ...
+    //     setUser(JSON.parse(localStorage.getItem('profile')));
+    // }, []);
     return (
         <>
         <AppBar className={classes.appBar} position="static" color="inherit">
@@ -29,7 +34,7 @@ const Navbar = () => {
                 <div className={classes.profile}>
                     <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageURL}>{user.result.name.charAt(0)}</Avatar>
                     <Typography className={classes.userName} variant='h6'>{user.result.name}</Typography>
-                    <Button variant='contained' className={classes.logout} color='secondary'>Logout</Button>
+                    <Button variant='contained' className={classes.logout} color='secondary' onClick={logout}>Logout</Button>
                 </div>
             ) : (
                 <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
