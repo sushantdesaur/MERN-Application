461ef7c43d29e4edb5093e4e98ef34069cee8092
sushantdesaur <desaursushant@gmail.com>
2022-01-07 11:54:59 +0530

    fixed errors in user creation * users was used in routes instead of user

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,10 +1,14 @@
 import { AUTH } from '../constants/actionTypes'
-import * as api from '../index.js'
+import * as api from '../api/index.js'
 
 export const signin = (formData, navigate) => async (dispatch) =>  {
     try {
         // sign in the user
-        navigate('/')
+        const { data } = await api.signIn(formData);
+
+        dispatch({ type: AUTH, data  });
+         
+        navigate('/');
 
     } catch (error) {
         console.log(error)
@@ -14,6 +18,8 @@ export const signin = (formData, navigate) => async (dispatch) =>  {
 export const signup = (formData, navigate) => async (dispatch) =>  {
     try {
         // sign up the user
+        const { data } = await api.signUp(formData)
+        dispatch({ type: AUTH, data }); 
         navigate('/')
 
     } catch (error) {
--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-app.use('/user', postRoutes);
+app.use('/user', userRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://DreamUser:LRypeyiCXsHiJ2d@cluster0.gkzil.mongodb.net/AuthenticationApp?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
